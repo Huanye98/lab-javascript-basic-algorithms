85f346062a104f83a32d2e837962e2e89c9241ac
Huanye Zhu <zhuzhenghuanye@gmail.com>
2024-06-19 17:46:45 +0200

    del 1 al 3

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // Iteration 1: Names and Input
+const hacker1 = "Huanye"
+console.log(`The driver's name is ${hacker1}`)
+
+const hacker2 = "SheylaGermán"
+console.log(`The navigator's name is ${hacker2}`)
 
 
 // Iteration 2: Conditionals
 
 
+if (hacker1.length === hacker2.length){
+    console.log(`Wow, you both have equally long names,${hacker2.length} characters!`)
+}else if(hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}else{
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
+
+
 // Iteration 3: Loops
+let result = "" 
+
+for(let i = 0; i<hacker1.length;i++){
+    result += hacker1[i].toUpperCase() + " "
+}
+console.log(result)
+
+let reverse = ""
+for(let l = hacker2.length-1; l>=0 ;l--){
+    reverse += hacker2[l]
+}
+console.log(reverse)
+
+
+
+
+
+console.log(hacker1>hacker2)
+
+
+
+for (let x = 0; x< hacker2.length;x++){
+    console.log("vuelta")
+    if   (hacker1[x] == hacker2[x]){
+     continue
+    }else if(hacker1[x] < hacker2[x]){
+        console.log("el lexicographic order de hacker2 es más grande")
+        break
+    }else if(hacker1[x] > hacker2[x]){
+        console.log("el lexicographic order de hacker1 es más grande")
+        break
+    }
+ 
+}
+
+
